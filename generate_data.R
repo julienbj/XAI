c410e92656ff8f3ddfd08110323c1ce4ceddbe02
Julie Natland Bjørnstad <julienbj@ulrik.uio.no>
2025-04-04 15:12:42 +0200

    Add code for dependence in generate_data

--- generate_data.R
+++ generate_data.R
@@ -1,13 +1,19 @@
 library(mvtnorm)
 
-generate_data = function(N, p=5, dependence=F){
+generate_data = function(N, p=5, dependence=F, rho=0.5){
   mu = rep(0, p)
-  Sigma = diag(p)
+  
+  if (dependence){
+    Sigma = matrix(rho, nrow=p, ncol=p)
+    diag(Sigma) = 1 
+  } else{
+    Sigma = diag(p) 
+  }
   
   X = rmvnorm(n = N, sigma = Sigma)
   y = 5 + X[,1] + 3*X[,2] + X[,3] + 2*X[,4] + 0.5*X[,5] + rnorm(N)
   return(list(X, y))
 }
 
-test = generate_data(10)
-print(dim(test[1])) 
+test = generate_data(10, dependence=TRUE)
+print(cor(test[[1]])) 
