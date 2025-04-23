4fd4388eb993fdf10ce6327955588de832b65e7b
Julie Natland Bjørnstad <julienbj@ulrik.uio.no>
2025-04-23 19:15:37 +0200

    Functioning simulation with independence and shapr

--- functions/perform_simulation.R
+++ functions/perform_simulation.R
@@ -4,26 +4,34 @@ source("functions/compute_shap.R")
 perform_simulation <- function(N=1000, M, dependence=F, include.shapr=T){
   train <- generate_data(N, M, dependence = dependence)
   test <- generate_data(500, M, dependence = dependence)
-  
-  X_test <- test$data[, 1:M]
-  
-  model = lm(y ~ ., data = train$data) 
+
+  X_train <- train$data[, grep("^V", names(train$data)), drop = FALSE]
+  X_test <- test$data[, grep("^V", names(test$data)), drop = FALSE]
+
+  model = lm(y ~ ., data = train$data)
   true_shap <- compute_shap(model=model, test_data = X_test, train=train, dependence=dependence)
-  
+
   mae <- list()
-  
+
   if (include.shapr) {
     library(shapr)
-    
-    explainer <- shapr(train$data, model)
-    shapr_vals <- explain(X_test, explainer, approach = "empirical")$shapley_values
-    shapr_vals <- shapr_vals[, colnames(X_test)]
-    
-    colnames(true_shap) <- colnames(shapr_vals)
-    
-    mae$shapr <- mean(abs(as.matrix(shapr_vals) - true_shap))
+
+    p0 <- mean(train$data$y)
+
+    explanation <- explain(
+      model = model,
+      x_explain = X_test,
+      x_train = X_train,
+      approach = "empirical",
+      phi0 = p0
+    )
+
+    shapr_vals <- explanation$shapley_values_est
+    shapr_matrix <- as.matrix(shapr_vals[, .(V1, V2, V3, V4, V5)])
+
+    mae$shapr <- mean(abs(shapr_matrix - true_shap))
   }
-  
+
   return(mae)
 }
 
