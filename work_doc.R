d3e294c19fbdc210eebde33bc0a6462df026b077
Julie Natland Bjørnstad <julienbj@ulrik.uio.no>
2025-05-14 12:33:17 +0200

    Minor restructures

--- work_doc.R
+++
@@ -1,9 +0,0 @@
-source("functions/run_simulation.R")
-source("functions/plot_results.R")
-
-betas1 <- c(0.3, 0.1, 0.3, -0.5, 0.7)
-betas2 <- c(3, 1, 3, -5, 7)
-
-run_sim(betas=betas1, filename="betas1_runs50_N1000")
-
-run_sim(betas=betas2, filename="betas2_runs50_N1000")
