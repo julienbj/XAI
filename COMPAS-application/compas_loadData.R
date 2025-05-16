e45014605a03c235cdee0aedbb5148c891386e6d
julienbj <129078623+julienbj@users.noreply.github.com>
2025-05-16 13:53:27 +0200

    Update compas_loadData.R

--- COMPAS-application/compas_loadData.R
+++ COMPAS-application/compas_loadData.R
@@ -3,9 +3,10 @@ library(ranger)
 library(shapr)
 library(ggplot2)
 library(data.table)
-library(randomForest)   
+library(tidyverse)   
+library(parsnip)
 
-in# Load COMPAS dataset
+# Load COMPAS dataset
 url <- "https://raw.githubusercontent.com/propublica/compas-analysis/master/compas-scores-two-years.csv"
 compas <- fread(url)
 
@@ -23,20 +24,19 @@ compas[, race := as.factor(race)]
 compas[, high_risk := factor(high_risk, levels = c(0, 1))]
 
 # Can now divide into train/test data to train black box model (e.g., random forest?)
-y        <- compas$high_risk              
-x        <- compas[, !"high_risk"]          
-x$sex    <- droplevels(x$sex)      
-x$race   <- droplevels(x$race)
-
 set.seed(2025)
-train_idx <- sample(nrow(x), 0.8 * nrow(x))
-x_train   <- x[train_idx]
-x_test    <- x[-train_idx]
-y_train   <- y[train_idx]
-y_test    <- y[-train_idx]
+train_idx <- sample(nrow(compas), 0.8 * nrow(compas))
+train   <- compas[train_idx]
+test    <- compas[-train_idx]
 
+rf_fit <- rand_forest() %>% set_engine("ranger") %>% set_mode("classification") %>% fit(high_risk ~ ., data=train)
 
 # Use shapr to explain predictions for test data. 
+explain_set <- train$high_risk[1:10]
+  
+train_prob <- predict(rf_fit, new_data=train, type="prob")$".pred_1"
+p0 <- mean(train_prob)
 
+explaination_independence <- explain(model = rf_fit$fit, x_explain=explain_set, x_train = train[, !"high_risk"], approach="independence", phi0=p0)
 
 # Might try a small number of test samples to make it less time-consuming
